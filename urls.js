131134d518a8dbcbce09368e166de70e5d460e5e
markpakingan <116990353+markpakingan@users.noreply.github.com>
2023-05-27 20:46:48 -0700

    added codes with html data per url

--- urls.js
+++ urls.js
@@ -1,3 +1,45 @@
+// const { URL } = require('url');
+// const fs = require('fs');
+// const axios = require('axios');
+
+// function readFile(path) {
+//   fs.readFile(path, 'utf-8', (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(data);
+//   });
+// }
+
+
+
+// readFile(path, (data) => {
+//   const urls = data.split('\n');
+//   console.log(urls);
+
+//   async function processURLs() {
+//     for (const url of urls) {
+//       try {
+//         const response = await axios.get(url);
+//         // const urlObj = new URL(url);
+//         // const hostname = urlObj.hostname;
+//         // console.log("hostname:", hostname);
+//         console.log(response.data);
+//       } catch (error) {
+//         console.error(`Error fetching URL: ${url}`);
+//         console.error(error);
+//       }
+//     }
+//   }
+
+//   processURLs();
+// });
+
+
+// const path = process.argv[2];
+
+
 const { URL } = require('url');
 const fs = require('fs');
 const axios = require('axios');
@@ -8,31 +50,38 @@ function readFile(path) {
       console.error(err);
       return;
     }
-    console.log(data);
+    processURLs(data);
   });
 }
 
-const path = process.argv[2];
-
-readFile(path, (data) => {
+function processURLs(data) {
   const urls = data.split('\n');
-  console.log(urls);
 
-  async function processURLs() {
+  async function fetchData(url) {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.error(`Error fetching URL: ${url}`);
+      console.error(error);
+      return null;
+    }
+  }
+
+  async function processAllURLs() {
     for (const url of urls) {
-      try {
-        const response = await axios.get(url);
-        // const urlObj = new URL(url);
-        // const hostname = urlObj.hostname;
-        // console.log("hostname:", hostname);
-        console.log(response.data);
-      } catch (error) {
-        console.error(`Error fetching URL: ${url}`);
-        console.error(error);
+      const html = await fetchData(url);
+      if (html) {
+        console.log(html);
+        // Process the HTML content here as needed
       }
     }
   }
 
-  processURLs();
-});
+  processAllURLs();
+}
 
+
+
+const path = process.argv[2];
+readFile(path);
